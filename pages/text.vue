9529462b2bbe5b5ad64510176e957b4a227c2ff1
ZhangHan412 <1455592102@qq.com>
2023-02-17 10:48:14 +0800

    第二个分支

--- pages/text.vue
+++ pages/text.vue
@@ -6,8 +6,6 @@
 
 <script>
 export default {
-    name: 'BcGameText1',
-
     data() {
         return {
             
--- pages/texte1.vue
+++ pages/texte1.vue
@@ -6,8 +6,6 @@
 
 <script>
 export default {
-    name: 'BcGameTexte',
-
     data() {
         return {
             
